c41e62159b450278dec51f2a8f31a7ef9a789e47
Beramos <bram.de.jaegher@gmail.com>
2017-10-31 23:34:40 +0100

    removed gray overlay

--- index.php
+++ index.php
@@ -3,13 +3,18 @@
 
   <section class="background">
     <div class="content-wrapper">
-      <img style="max-width:80%;margin:auto;" class="img-responsive center-block wrapper coverImage" src="figs/dingdongsCover.png">
+      <img style="max-width:80%;margin:auto;" class="img-responsive center-block wrapper coverImage" src="figs/dingdongsCoverNoName.png">
     </div>
   </section>
   <section class="background">
     <div class="content-wrapper">
-      <p class="content-title">Cras lacinia non eros nec semper.</p>
-      <p class="content-subtitle">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras ut massa mattis nibh semper pretium.</p>
+     <img style="max-width:80%;margin:auto;" class="img-responsive center-block wrapper coverImage" src="figs/14-fietslever-dingdong-v2_noBackground.png">
+    </div>
+  </section>
+  <section class="background">
+    <div class="content-wrapper">
+      <p class="content-title">Etiam consequat lectus.</p>
+      <p class="content-subtitle">Nullam tristique urna sed tellus ornare congue. Etiam vitae erat at nibh aliquam dapibus.</p>
     </div>
   </section>
   <section class="background">
