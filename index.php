0143bf2a30c619377aec44c3d2ea21af94aae5e1
Beramos <bram.de.jaegher@gmail.com>
2017-11-01 01:11:27 +0100

    included second and third page

--- index.php
+++ index.php
@@ -8,13 +8,37 @@
   </section>
   <section class="background">
     <div class="content-wrapper">
-     <img style="max-width:80%;margin:auto;" class="img-responsive center-block wrapper coverImage" src="figs/14-fietslever-dingdong-v2_noBackground.png">
+     <div id="definition">
+        <p>DING . DONG (de; m; meervoud: dingdongs)</p>    
+        <p style="font-size:40px;"><sup>1</sup>geluid van een deurbel</p>    
+        <p style="font-size:40px;"><sup>2</sup>jonge, leuk uitziende kerel</p>    
+    </div>
+     <img style="max-width:80%;margin:auto;" class="img-responsive center-block wrapper secondImage" src="figs/14-fietslever-dingdong-v2_noBackground.png">
     </div>
   </section>
   <section class="background">
     <div class="content-wrapper">
-      <p class="content-title">Etiam consequat lectus.</p>
-      <p class="content-subtitle">Nullam tristique urna sed tellus ornare congue. Etiam vitae erat at nibh aliquam dapibus.</p>
+        <div class="row" style="margin-left:40px;"> 
+          <div class="column">
+              <div class="row">
+                <img src="figs/dude.png">
+                <p class="nameDude"> SHENAN STU </p>
+                <img src="figs/dude.png">
+                <p class="nameDude"> SWAN FRY </p>
+              </div>
+          </div>
+          <div class="column">
+              <div class="row">
+                <img src="figs/dude.png">
+                <p class="nameDude"> HAUCK KEETON </p>
+                <img src="figs/dude.png">
+                <p class="nameDude"> MITCH COE </p>
+              </div>
+          </div>
+            <div class="column" style="width:50%;">
+            <div style="background-color:gray;width:100;height:1000px;"> Wat moet hier?</div>
+          </div>
+        </div>
     </div>
   </section>
   <section class="background">
