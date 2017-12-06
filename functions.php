3958c1feab618682f59637728b2bb0115a785c02
Beramos <bram.de.jaegher@gmail.com>
2017-12-06 20:30:12 +0100

    removed commented code

--- functions.php
+++ functions.php
@@ -48,45 +48,6 @@ remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wr
       echo '</ul>';
     }
 
-
-/*
-    add_filter('woocommerce_add_to_cart_redirect', 'theme_prefix_add_to_cart_redirect');
-    function theme_prefix_add_to_cart_redirect( $url ) {
-         $url = wc_get_checkout_url();
-        return  $url;
-    }
-
-    add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text' ); // 2.1 +
-    function woo_custom_cart_button_text() {
-        return __( 'Proceed to Checkout', 'woocommerce' );
-    }
-
-    add_filter( 'woocommerce_product_add_to_cart_text', 'woo_archive_custom_cart_button_text' ); // 2.1 +
-    function woo_archive_custom_cart_button_text() {
-        return __( 'Proceed to Checkout', 'woocommerce' );
-    }
-*/
-
-/*
-add_filter( 'woocommerce_loop_add_to_cart_link', 'quantity_inputs_for_woocommerce_loop_add_to_cart_link', 10, 2 );
-function quantity_inputs_for_woocommerce_loop_add_to_cart_link( $html, $product ) {
-	if ( $product && $product->is_type( 'simple' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
-		$html = '<form action="' . esc_url( $product->add_to_cart_url() ) . '" class="cart" method="post" enctype="multipart/form-data">';
-		$html .= woocommerce_quantity_input( array(), $product, false );
-		$html .= '<button type="submit" class="button alt">' . esc_html( $product->add_to_cart_text() ) . '</button>';
-		$html .= '</form>';
-	}
-	return $html;
-}
-*/
-
-/*add_filter('woocommerce_add_to_cart_redirect', 'themeprefix_add_to_cart_redirect');
-function themeprefix_add_to_cart_redirect() {
- global $woocommerce;
- $checkout_url = wc_get_checkout_url();
- return $checkout_url;
-}*/
-
 add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
 
 /* Remove deliver to a different Addres*/
