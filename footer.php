06daa97ea16fe20fb6709d51b28fa1587f26573a
Beramos <bram.de.jaegher@gmail.com>
2017-12-07 19:12:36 +0100

    fixed cursor pointer on navbar and navbullets

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
       <div id="fp-nav" class="right" style="color: rgb(0, 0, 0);">
           <ul>
-              <li style="margin-top:0;" class="navBullet active"><a href="#nicer_way"><span></span></a></li>
-              <li class="navBullet"><a href="#nicer_way"><span></span></a></li>
-              <li class="navBullet"><a href="#nicer_way" ><span></span></a></li>
-              <li class="navBullet"><a href="#nicer_way" ><span></span></a></li>
+              <li style="margin-top:0;" class="navBullet active"><p><span></span></p></li>
+              <li class="navBullet"><p><span></span></p></li>
+              <li class="navBullet"><p><span></span></p></li>
+              <li class="navBullet"><p><span></span></p></li>
           </ul>
       </div>
 
