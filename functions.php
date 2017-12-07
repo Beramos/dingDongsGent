d084ad96f6ff174b811af9397ee7fac5937dc000
Beramos <bram.de.jaegher@gmail.com>
2017-12-07 19:03:29 +0100

    removed anchor from product

--- functions.php
+++ functions.php
@@ -137,4 +137,8 @@ function custom_woo_after_shop_loop() {
         array('jquery'));
   wp_enqueue_script( 'alerter');
 }
+
+// Remove anchor from product //
+remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
 ?>
