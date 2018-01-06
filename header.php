e9abc6ec27efa81971d9c5d30d938036c02d043c
brdjaegh <bram.dejaegher@UGent.be>
2018-01-06 15:11:55 +0100

    work on footer

--- header.php
+++ header.php
@@ -21,7 +21,9 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-   
+    <script src="https://use.fontawesome.com/667f057fde.js"></script>
+
+      
     <?php wp_enqueue_script("jquery"); ?>
     <?php wp_head(); ?>
   </head>
